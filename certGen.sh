7fec0e0b5ce84c017ac8caa4e12651798e4921ce
edan184 <edan184@hotmail.com>
2023-09-29 06:58:34 -0700

    changed case of variables

--- certGen.sh
+++ certGen.sh
@@ -2,8 +2,8 @@ mkdir -p certs; mkdir -p keys; mkdir -p requests; mkdir -p demoCA/newcerts; touc
 if [ ! -e "demoCA/serial" ]; then 
     touch demoCA/serial && echo 00 > demoCA/serial
 fi
-read -p "Enter hostname certificate names delineated by single spaces: " host
-hosts=(${host})
+read -p "Enter hostname certificate names delineated by single spaces: " Host
+Hosts=(${Host})
 read -p "Enter single domain name to suffix end of local hostname: " Domain
 
 
@@ -17,14 +17,14 @@ read -p "Enter certificate city: " L
 read -p "Enter certificate organization: " O
 read -p "Enter certificate department: " OU
 
-for ((i=0; i < ${#hosts[@]}; i++));
+for ((i=0; i < ${#Hosts[@]}; i++));
 
     do 
+        echo "Processing certificate for ${Hosts[$i]}${Domain}"
+        openssl req -new -x509 -days 3650 -keyout keys/ca-${Hosts[$i]}${Domain}.key -out certs/ca-${Hosts[$i]}${Domain}.cer -subj "/C=$C/ST=$ST/L=$L/O=$O/OU=$OU/CN=${Hosts[$i]}${Domain}"
 
-        openssl req -new -x509 -days 3650 -keyout keys/ca-${hosts[$i]}${Domain}.key -out certs/ca-${hosts[$i]}${Domain}.cer -subj "/C=$C/ST=$ST/L=$L/O=$O/OU=$OU/CN=${hosts[$i]}${Domain}"
+        openssl req -new -newkey rsa:2048 -nodes -keyout keys/${Hosts[$i]}${Domain}.key -out requests/${Hosts[$i]}${Domain}.csr -subj "/C=$C/ST=$ST/L=$L/O=$O/OU=$OU/CN=${Hosts[$i]}${Domain}"
 
-        openssl req -new -newkey rsa:2048 -nodes -keyout keys/${hosts[$i]}${Domain}.key -out requests/${hosts[$i]}${Domain}.csr -subj "/C=$C/ST=$ST/L=$L/O=$O/OU=$OU/CN=${hosts[$i]}${Domain}"
-
-        openssl ca -policy policy_anything -cert certs/ca-${hosts[$i]}${Domain}.cer -in requests/${hosts[$i]}${Domain}.csr -keyfile keys/ca-${hosts[$i]}${Domain}.key -days 365 -out certs/${hosts[$i]}${Domain}.cer
+        openssl ca -policy policy_anything -cert certs/ca-${Hosts[$i]}${Domain}.cer -in requests/${Hosts[$i]}${Domain}.csr -keyfile keys/ca-${Hosts[$i]}${Domain}.key -days 365 -out certs/${Hosts[$i]}${Domain}.cer
 
     done
